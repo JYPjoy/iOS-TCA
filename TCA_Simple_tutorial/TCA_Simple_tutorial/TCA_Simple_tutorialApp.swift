6b8a8723fc6e6ceef480228674a67bdc1c7216b0
JOY <jypjoy@gmail.com>
2023-05-05 15:19:09 +0900

    ✨ feat: 파일 변경

---
+++ TCA_Simple_tutorial/TCA_Simple_tutorial/TCA_Simple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Simple_tutorialApp.swift
+//  TCA_Simple_tutorial
+//
+//  Created by Joy on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Simple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
